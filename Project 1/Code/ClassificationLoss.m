be61a026df1a962e882658121fd0a6f808e017d1
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-03-22 14:40:26 +0800

    Add files via upload

---
+++ Project 1/Code/ClassificationLoss.m
@@ -0,0 +1,54 @@
+function [loss, grad] = ...
+    ClassificationLoss(weights, X, y, nHidden, nLabel)
+
+    [n,dim] = size(X);
+    [weightsInput, weightsHidden, weightsOutput] = ...
+        FormWeights(weights, dim, nHidden, nLabel);
+
+    loss = 0;
+    if nargout > 1
+        [gradInput, gradHidden, gradOutput] = ...
+            InitializeGrad(weightsInput, weightsHidden, weightsOutput, ...
+            nHidden);
+    end
+
+    % compute output
+    netActivation = cell(length(nHidden), 1);
+    Activation = cell(length(nHidden), 1);
+    for indexInput = 1:n
+        netActivation{1} = X(indexInput, :) * weightsInput;
+        Activation{1} = tanh(netActivation{1});
+        for indexHidden = 2:length(nHidden)
+            netActivation{indexHidden} = Activation{indexHidden - 1} * ...
+                weightsHidden{indexHidden - 1};
+            Activation{indexHidden} = tanh(netActivation{indexHidden});
+        end
+        yPred = Activation{end} * weightsOutput;
+        
+        error = yPred - y(indexInput, :);
+        loss = loss + sum(error .^ 2);
+        
+        if nargout > 1
+            % output layer
+            error = 2 * error;
+            gradOutput = gradOutput + Activation{end}' * error;
+            error = sech(netActivation{end}) .^ 2 .* ...
+                (error * weightsOutput');
+            % hidden layers
+            for indexHidden = length(nHidden) - 1: -1: 1
+                gradHidden{indexHidden} = gradHidden{indexHidden} + ...
+                    Activation{indexHidden}' * error;
+                error = (error * weightsHidden{indexHidden}') .*  ...
+                    sech(netActivation{indexHidden}) .^ 2;
+            end
+            % input layer
+            gradInput = gradInput + X(indexInput,:)' * error;
+        end
+    end
+
+    % put gradient into vector
+    if nargout > 1
+        grad = FormGrad(gradInput, gradHidden, gradOutput, ...
+            weights, dim, nHidden, nLabel);
+    end
+end
