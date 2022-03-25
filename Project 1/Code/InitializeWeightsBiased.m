2ff3acaeb25fc9b85ffe9f5716c3ffd4c58eb035
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-03-25 20:49:47 +0800

    Delete Project 1/Code directory

--- Project 1/Code/InitializeWeightsBiased.m
+++
@@ -1,8 +0,0 @@
-function weights = InitializeWeightsBiased(dim, nHidden, nLabel)
-    nParams = dim * (nHidden(1) - 1);
-    for h = 2: length(nHidden)
-        nParams = nParams + nHidden(h - 1) * (nHidden(h) - 1);
-    end
-    nParams = nParams + nHidden(end) * nLabel;
-    weights = randn(nParams, 1);
-end
