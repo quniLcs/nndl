597176c0b52160e37bad896067a909da00010684
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-03-26 16:25:58 +0800

    Delete Project 1/Code directory

--- Project 1/Code/InitializeWeightsBasic.m
+++
@@ -1,8 +0,0 @@
-function weightLinear = InitializeWeightsBasic(dim, nHidden, nLabel)
-    nParams = dim * nHidden(1);
-    for h = 2: length(nHidden)
-        nParams = nParams + nHidden(h - 1) * nHidden(h);
-    end
-    nParams = nParams + nHidden(end) * nLabel;
-    weightLinear = randn(nParams, 1);
-end
--- Project 1/Code/InitializeWeightsBiased.m
+++
@@ -1,8 +0,0 @@
-function weightLinear = InitializeWeightsBiased(dim, nHidden, nLabel)
-    nParams = dim * (nHidden(1) - 1);
-    for h = 2: length(nHidden)
-        nParams = nParams + nHidden(h - 1) * (nHidden(h) - 1);
-    end
-    nParams = nParams + nHidden(end) * nLabel;
-    weightLinear = randn(nParams, 1);
-end
