9b916fe2c4bd6087708d500ebfe7cfaff1d9143f
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-04-10 09:14:09 +0800

    Add set_random_seeds()

--- Project 2/Code/util.py
+++ Project 2/Code/util.py
@@ -1,4 +1,17 @@
+import random
 import numpy as np
+import torch
+
+
+def set_random_seeds(seed = 0, device = 'cpu'):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if device != 'cpu':
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
 
 def get_num_parameters(model):
