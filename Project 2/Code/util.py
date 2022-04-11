6ed8b82af6919290e69934bdfec878368349c417
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-04-11 20:56:15 +0800

    add loss_landscape()

--- Project 2/Code/util.py
+++ Project 2/Code/util.py
@@ -105,3 +105,21 @@ def train(model, optimizer, criterion, train_loader, test_loader, num_epochs = 2
         torch.save(test_errors, test_errors_file)
 
     return losses, train_errors, test_errors
+
+
+def loss_landscape(losses):
+    min_curve = losses[0]
+    max_curve = losses[0]
+
+    for ind in range(1, len(losses)):
+        for ind_epoch in range(len(losses[0])):
+            for ind_batch in range(len(losses[0][0])):
+                if losses[ind][ind_epoch][ind_batch] < min_curve[ind_epoch][ind_batch]:
+                    min_curve[ind_epoch][ind_batch] = losses[ind][ind_epoch][ind_batch]
+                elif losses[ind][ind_epoch][ind_batch] > max_curve[ind_epoch][ind_batch]:
+                    max_curve[ind_epoch][ind_batch] = losses[ind][ind_epoch][ind_batch]
+
+    min_curve = [value for sub_curve in min_curve for value in sub_curve]
+    max_curve = [value for sub_curve in max_curve for value in sub_curve]
+
+    return min_curve, max_curve
